0933f4247d5adb4b8b2e387b22607d623f67f2eb
Goricanec David <David.Goricanec@andritz.com>
2020-06-01 21:56:20 +0200

    2

--- Bandfinder_Project/app/src/main/java/at/jhj/bandfinder_project/MainActivity.kt
+++ Bandfinder_Project/app/src/main/java/at/jhj/bandfinder_project/MainActivity.kt
@@ -1,23 +1,45 @@
 package at.jhj.bandfinder_project
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btn_secondPage: Button;
+    lateinit var btn_verify: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_secondPage = findViewById(R.id.btnSecondPage)
 
-        btn_secondPage.setOnClickListener{
-            val intent: Intent = Intent(applicationContext,PersonDataActivity::class.java)
-            startActivity(intent)
+        btn_verify = findViewById(R.id.btn_verify)
+
+
+        btn_verify.setOnClickListener{
+
+            var phoneumber: String = findViewById<TextView>(R.id.txtNummer).text.toString()
+            var code: String = findViewById<TextView>(R.id.txt_code).text.toString()
+
+            phoneumber = code + phoneumber
+
+
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneumber, // Phone number to verify
+                60, // Timeout duration
+                TimeUnit.SECONDS, // Unit of timeout
+                this, // Activity (for callback binding)
+                callbacks) // OnVerificationStateChangedCallbacks
+
+            //val intent: Intent = Intent(applicationContext,PersonDataActivity::class.java)
+            //startActivity(intent)
         }
     }
 
